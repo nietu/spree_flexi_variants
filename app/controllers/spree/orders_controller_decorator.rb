f612fc5739ce42285bc22c1f0c921ac7b9a3db6c
Tommi Niemi <nietu@pcuf.fi>
2015-09-24 15:43:11 +0300

    full custom product

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -13,6 +13,7 @@ module Spree
       params[:options][:ad_hoc_option_values] = ad_hoc_option_value_ids
       params[:options][:product_customizations] = product_customizations
       params[:options][:customization_price] = params[:customization_price] if params[:customization_price]
+      params[:options][:design_id] = params[:design_id] if params[:design_id]
     end
 
   end
